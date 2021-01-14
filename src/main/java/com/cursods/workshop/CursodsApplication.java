be90066ff5ded60976e880a065ebdc5d56cbdfa2
PedroSpiet <pedro.silva186@fatec.sp.gov.br>
2021-01-14 14:35:24 -0300

    Initial Commit

---
+++ src/main/java/com/cursods/workshop/CursodsApplication.java
@@ -0,0 +1,13 @@
+package com.cursods.workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursodsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursodsApplication.class, args);
+	}
+
+}
