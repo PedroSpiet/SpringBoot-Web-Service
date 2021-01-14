4e82bc1afc79a2f209ca715e0ec024158fb44144
PedroSpiet <pedro.silva186@fatec.sp.gov.br>
2021-01-14 18:51:08 -0300

    Service layer and component registration

--- src/main/java/com/cursods/workshop/resources/UserResources.java
+++ src/main/java/com/cursods/workshop/resources/UserResources.java
@@ -1,19 +1,33 @@
 package com.cursods.workshop.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cursods.workshop.entities.User;
+import com.cursods.workshop.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResources {
 	
+	@Autowired
+	private UserService userService;
+	
 	@GetMapping
-	public ResponseEntity<User> FindUsers() {
-		User users = new User(1L, "Pedro", "99999", "Pedro@gmail.com", "12345");
+	public ResponseEntity<List<User>> index() {
+		List<User> users = userService.findAll();
 		return ResponseEntity.ok().body(users);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> show(@PathVariable Long id) {
+		User user = userService.FindOne(id);
+		return ResponseEntity.ok().body(user);
 	}
 }
